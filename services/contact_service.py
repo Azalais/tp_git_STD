b290241e1561e32d8a7d68130fef5dddfb0c9861
Azalais <eline.chartier@orange.fr>
2025-01-06 16:38:29 +0100

    Ajout de la fonctionnalité pour supprimer des contacts

--- services/contact_service.py
+++ services/contact_service.py
@@ -11,5 +11,12 @@ class ContactService:
         self.contacts.append(contact)
         return f"Contact {name} ajouté."
 
+    def suppr_contact(self, name: str) -> str:
+        for contact in self.contacts:
+            if contact.name == name:
+                self.contacts.remove(contact)
+                return f"Contact {name} supprimé."
+        return f"Contact {name} introuvable"
+
     def get_contacts(self) -> List[Contact]:
         return self.contacts
